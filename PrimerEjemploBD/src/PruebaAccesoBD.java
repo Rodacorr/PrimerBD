8b3fbdbb55d4a472fee54275ec70d1b09be3c779
matip <matip@MATIDESKTOP>
2024-08-23 09:13:53 -0300

    Commit

--- PrimerEjemploBD/src/PruebaAccesoBD.java
+++ PrimerEjemploBD/src/PruebaAccesoBD.java
@@ -17,15 +17,10 @@ public class PruebaAccesoBD
 	{
 		try
 		{
-			/* primer programa de prueba para ejemplo de acceso a MySQL desde Java */
-			/* accede a una base de datos de MySQL llamada Prueba que contiene una tabla llamada Personas */
-			/* dentro de dicha tabla hay una columna llamada nombre */
-
-			/* 1. cargo dinamicamente el driver de MySQL */
+			
 			String driver = "com.mysql.jdbc.Driver";
 			Class.forName(driver);
 
-			/* 2. una vez cargado el driver, me conecto con la base de datos */
 			Properties prop = new Properties();
 			String nomArch = "src/config/datos.dat";
 			try {
@@ -49,23 +44,19 @@ public class PruebaAccesoBD
 			Connection con = DriverManager.getConnection(url, usr, pwd);
 			
 			System.out.println ("Login completo...");
-
-			/* 3. creo un PreparedStatement para insertar una persona en base de datos */
+	
 			String insert = "INSERT INTO Personas" + "(Nombre)" + "VALUES (?)";
 			PreparedStatement pstmt = con.prepareStatement(insert);
 			pstmt.setString(1, "Jose");
 
-			/* 4. ejecuto la sentencia de insercion y cierro el PreparedStatement */
 			int cant = pstmt.executeUpdate();
 			pstmt.close();
 			System.out.print("Resultado de " + insert + ": ");
 			System.out.println(cant + " filas afectadas");
 
-			/* 5. creo un Statement para listar todas las personas de la base de datos */
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM Personas";
 
-			/* 6. ejecuto la consulta, listo las personas y cierro el ResultSet y el Statement */
 			ResultSet rs = stmt.executeQuery(query);
 			System.out.println("Resultado de " + query);
 			while (rs.next())
@@ -75,7 +66,6 @@ public class PruebaAccesoBD
 			rs.close();
 			stmt.close();
 
-			/* 7. por ultimo, cierro la conexion con la base de datos */
 			con.close();
 		}
 		catch (ClassNotFoundException e)
